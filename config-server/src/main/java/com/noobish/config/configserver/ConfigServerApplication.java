ef5b6fbd9b0aff3847af52404404b92e79b86f96
ibhi <ibrahimin2004@gmail.com>
2018-02-12 20:54:10 +0530

    wip: eureka and config are working in aws ec2

--- config-server/src/main/java/com/noobish/config/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/noobish/config/configserver/ConfigServerApplication.java
@@ -1,9 +1,14 @@
 package com.noobish.config.configserver;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableConfigServer
 @EnableDiscoveryClient
@@ -12,5 +17,14 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Bean
+	@Profile("aws")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		config.setDataCenterInfo(info);
+		return config;
 	}
 }
